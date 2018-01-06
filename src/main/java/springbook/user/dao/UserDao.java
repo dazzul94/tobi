f7b94b9d298083e0a69a80f989b383d8ecfcb6a5
dazzul94 <dazzul@naver.com>
2018-01-06 19:44:01 +0900

    .

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -1,7 +1,6 @@
 package springbook.user.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,8 +8,14 @@ import java.sql.SQLException;
 import springbook.user.domain.User;
 
 public class UserDao {
+    private ConnectionMaker connectionMaker;
+    
+    public UserDao(ConnectionMaker connectionMaker) {
+    this.connectionMaker = connectionMaker;    
+    }
+    
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = connectionMaker.makeNewConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
@@ -23,7 +28,7 @@ public class UserDao {
     }
     
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = connectionMaker.makeNewConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id =?");
         ps.setString(1, id);
         
@@ -39,29 +44,5 @@ public class UserDao {
         c.close();
         
         return user;
-    }
-    private Connection getConnection() throws ClassNotFoundException, SQLException { 
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/tobi", "study", "1111");
-        return c;
-        
-    }
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new UserDao();
-        
-        User user = new User();
-        user.setId("dazzul");
-        user.setName("김다솔");
-        user.setPassword("1111");
-        
-        dao.add(user);
-        
-        System.out.println(user.getId() + " 등록 성공");
-        
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-        
-        System.out.println("조회 성공!");
     }
 }
